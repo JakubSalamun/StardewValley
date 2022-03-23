e9d85eeea393728dd9812ee604fa670e0ba3e140
Askalt <jakub.salamun98@gmail.com>
2022-03-23 15:17:20 +0100

    Add project files.

---
+++ StardewValley/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StardewValley
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void TV_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TV News");
+        }
+
+        private void Calendar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Day of Year");
+        }
+
+        private void Bed_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sleep");
+        }
+    }
+}
